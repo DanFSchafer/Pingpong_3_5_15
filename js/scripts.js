423c2f5f79a7361a43ae71090d6a200aff29fc6b
Dan Schafer <danfschafer@gmail.com>
2016-06-23 15:37:25 -0700

    Transform input number into a 1-to-n array, and display the array elements as an unordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 
+var arrays1 = [];
+
 $(function() {
   $("#input form").submit(function(event) {
     event.preventDefault();
-    var inputNum = $("input#inputNum").val();
+    var inputNum = parseInt($("input#inputNum").val());
     console.log(inputNum);
 
-    // $(".person1").text(person1Input);
+//create array of numbers
+    for (var index=1; index < inputNum + 1; index +=1) {
+      arrays1.push(index);
+    }
+    console.log(arrays1);
+
+//list array of numbers as a unordered list
+    arrays1.forEach(function(array1) {
+      $("#theList").append("<li>" + array1 + "</li>");
+    });
+
 
     $("#result").show();
   });
