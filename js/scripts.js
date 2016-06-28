d880b82b399d377c5d2c1c1b18b7a4814917f6ee
Dan Schafer <danfschafer@gmail.com>
2016-06-27 23:03:08 -0700

    update array names, and remove deugging code

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,35 @@ $(function() {
     event.preventDefault();
 
     var inputNum = parseInt($("input#inputNum").val());
-//  console.log(inputNum);
 
-//create array of numbers
-    var arrays1 = [];
+//create array of integers from 1 to input
+    var numbers = [];
     for (var index=1; index < inputNum + 1; index +=1) {
-      arrays1.push(index);
+      numbers.push(index);
     }
-    //  console.log(arrays1);
 
     //replace array elements evenly divisible by 15 with "pingpong"
     //replace array elements evenly divisible by 3 with "ping"
     //replace array elements evenly divisible by 5 with "pong"
-    var arrays2 = arrays1.map(function(array1) {
-      if (array1 % 15 === 0) {
+    var pongNumbers = numbers.map(function(number) {
+      if (number % 15 === 0) {
         return "pingpong";
-      } else if (array1 % 3 === 0) {
+      } else if (number % 3 === 0) {
         return "ping";
-      } else if (array1 % 5 === 0) {
+      } else if (number % 5 === 0) {
         return "pong";
       } else {
-        return array1;
+        return number;
       }
     });
-
-// console.log(arrays2);
 
 //delete existing list for multiple submissions
     $("#result").hide();
     $("#theList").text("");
 
 //display resulting array elements as an unordered list
-    arrays2.forEach(function(array2) {
-      $("#theList").append("<li>" + array2 + "</li>");
+    pongNumbers.forEach(function(pongNumber) {
+      $("#theList").append("<li>" + pongNumber + "</li>");
     });
     $("body").css("background-image","url('img/pingpong1.png')");
     $("#result").fadeIn();
