10b56f21c99fc254401909c37dc3240e54efd046
Dan Schafer <danfschafer@gmail.com>
2016-06-28 12:20:14 -0700

    create function to generate final pong list, and place it above the jQuery code in the scripts.js file. Call function in jQuery code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,50 @@
+//javascript
+
+// a function to generate "pong" list
+var calcPong = function(input) {
+
+//create array of integers from 1 to input
+  var numbers = [];
+  for (var index=1; index < input + 1; index +=1) {
+    numbers.push(index);
+  };
+
+//replace array elements evenly divisible by 15 with "pingpong"
+//replace array elements evenly divisible by 3 with "ping"
+//replace array elements evenly divisible by 5 with "pong"
+  var pongNumbers = numbers.map(function(number) {
+  if (number % 15 === 0) {
+    return "pingpong";
+  } else if (number % 3 === 0) {
+    return "ping";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else {
+    return number;
+  }
+  });
+  return(pongNumbers)
+};
+
+// jQuery
 
 $(function() {
   $("#input form").submit(function(event) {
     event.preventDefault();
 
+//collect input integer from form
     var inputNum = parseInt($("input#inputNum").val());
 
-//create array of integers from 1 to input
-    var numbers = [];
-    for (var index=1; index < inputNum + 1; index +=1) {
-      numbers.push(index);
-    }
-
-    //replace array elements evenly divisible by 15 with "pingpong"
-    //replace array elements evenly divisible by 3 with "ping"
-    //replace array elements evenly divisible by 5 with "pong"
-    var pongNumbers = numbers.map(function(number) {
-      if (number % 15 === 0) {
-        return "pingpong";
-      } else if (number % 3 === 0) {
-        return "ping";
-      } else if (number % 5 === 0) {
-        return "pong";
-      } else {
-        return number;
-      }
-    });
+//generate pong list
+    var results = calcPong(inputNum);
 
 //delete existing list for multiple submissions
     $("#result").hide();
     $("#theList").text("");
 
 //display resulting array elements as an unordered list
-    pongNumbers.forEach(function(pongNumber) {
-      $("#theList").append("<li>" + pongNumber + "</li>");
+    results.forEach(function(result) {
+      $("#theList").append("<li>" + result + "</li>");
     });
     $("body").css("background-image","url('img/pingpong1.png')");
     $("#result").fadeIn();
